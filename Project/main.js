4beb4fe117458521c8067d45cca3f9e040b12c1f
shivani <98374589+Shivani505001@users.noreply.github.com>
2022-03-02 14:15:11 +0530

    Add files via upload

---
+++ Project/main.js
@@ -0,0 +1,9 @@
+function validation(){
+    var Usernmae = document.getElementById("Usernmae").value ;
+    var email =document.getElementById("email").value ;
+    var password =document.getElementById("password").value ;
+    var name=document.getElementById("").value ;
+    var name=document.getElementById("").value ;
+
+
+}
